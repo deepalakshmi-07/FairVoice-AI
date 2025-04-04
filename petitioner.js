ca33877afe371463f5b0dd4d00fb1b1175e5c0e2
Roshini315 <roshinithangavel9@gmail.com>
2025-04-04 11:55:37 +0530

    petition page

---
+++ petitioner.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("registerBtn").addEventListener("click", function () {
+      window.location.href = "subpetition.html";
+    });
+  });
+  
